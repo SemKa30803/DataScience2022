b4bd291efc7ebe2f4e0f2734e9db2b9b848896f1
semka30803 <semenkorolev2003@gmail.com>
2022-04-08 18:57:58 +0300

    first module 2

--- Module 1/Task1.py
+++ Module 1/Task1.py
@@ -1,2 +1,2 @@
-word = 't'
+word = 'testing'
 print(word[len(word) // 2] if len(word) % 2 != 0 else word[len(word) // 2 - 1] + word[len(word) // 2])
--- Module 1/Task7.py
+++ Module 1/Task7.py
@@ -1,12 +1,12 @@
 results = {
-'vk': {'revenue': 103, 'cost': 98},
-'yandex': {'revenue': 179, 'cost': 153},
-'facebook': {'revenue': 103, 'cost': 110},
-'adwords': {'revenue': 35, 'cost': 34},
-'twitter': {'revenue': 11, 'cost': 24},
+    'vk': {'revenue': 103, 'cost': 98},
+    'yandex': {'revenue': 179, 'cost': 153},
+    'facebook': {'revenue': 103, 'cost': 110},
+    'adwords': {'revenue': 35, 'cost': 34},
+    'twitter': {'revenue': 11, 'cost': 24},
 }
 
 for i in results:
-    print (i)
-    results[i]['ROI'] = round((results[i]['revenue']/results[i]['cost'] - 1)*100*100)/100
+    # print (i)
+    results[i]['ROI'] = round((results[i]['revenue'] / results[i]['cost'] - 1) * 100 * 100) / 100
 print(results)
